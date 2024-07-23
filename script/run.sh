e811b93aaf6d6f5a13e08b5c8671a884dca6440a
TanakAiko <cherifmbaye02@gmail.com>
2024-07-23 12:23:37 +0000

    Message done !

--- script/run.sh
+++ script/run.sh
@@ -1,7 +1,4 @@
-docker rm -f $(docker ps -aq)
-docker volume rm $(docker volume ls -q)
-docker rmi -f $(docker images -q)
-
-echo "It might take a little longer for the first time\n"
-
-docker-compose up --build
+docker-compose stop frontend
+docker-compose rm -f frontend
+docker-compose build frontend
+docker-compose up -d frontend
